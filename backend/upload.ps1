0fb69c3c702a27705a55d936263fefc999811d68
fourtf <tf.four@gmail.com>
2021-10-17 15:39:06 +0200

    added script to push lambda function

---
+++ backend/upload.ps1
@@ -0,0 +1,3 @@
+zip tmp.zip -r --exclude="*.git*" --exclude="*.zip" . > $null
+aws s3 cp tmp.zip s3://hka-gruppe11-backend/fn.zip
+aws lambda update-function-code --function-name backend --region=us-east-1/tmp.zip --region=us-east-1 --s3-bucket=hka-gruppe11-backend --s3-key=fn.zip
