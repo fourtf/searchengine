7ee9e132d56d84c8edc063322167e115191f6ec0
fourtf <tf.four@gmail.com>
2021-10-17 15:10:51 +0200

    added MUI to frontend

---
+++ frontend/src/SearchComponent.tsx
@@ -0,0 +1,20 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import { Component } from "react";
+
+export default class SearchComponent extends Component<any, any> {
+  render() {
+    return (
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <TextField
+          InputProps={{
+            startAdornment: <SearchIcon />,
+          }}
+        >
+        </TextField>
+        <Button variant="outlined">Search</Button>
+      </div>
+    );
+  }
+}
