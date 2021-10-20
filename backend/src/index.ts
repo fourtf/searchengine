6562b181a7034a2a9c3e16bcb09e285a4f7ff655
fourtf <tf.four@gmail.com>
2021-10-20 22:35:41 +0200

    autoformat

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,24 +4,24 @@ import { Client } from "@elastic/elasticsearch";
 const client = new Client({ node: "http://node-1.hska.io:9200/" });
 
 async function count() {
-  const count = await client.count(
-    {
-      index: "songs",
-    },
-  );
+  const count = await client.count({
+    index: "songs",
+  });
 
   return count.body;
 }
 
 export const handler = async (
-  event: APIGatewayProxyEvent,
+  event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   const requestContext = event.requestContext;
-  const { http: { method, path } } = requestContext as any;
+  const {
+    http: { method, path },
+  } = requestContext as any;
 
   if (path === "/songs" && method === "GET") {
     return { statusCode: 200, body: JSON.stringify(await count()) };
-  } 
+  }
 
   return { statusCode: 200, body: JSON.stringify(event) };
 };
